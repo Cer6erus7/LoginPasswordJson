01a3953bdf7108c98d21f2e779a56c5524ecf82f
Matvii Golovko <cer6erus7@Matviis-Laptop.lan>
2023-07-21 12:19:15 -0700

    Some small changes

--- info.py
+++ info.py
@@ -1,5 +1,6 @@
 instruction = """
 /register - make a new account
 /login - login in the existing account
+/return - return back to main menu
 /help - know description of this app
-/end - end cession"""
+/end - close app"""
--- main.py
+++ main.py
@@ -5,12 +5,21 @@ import utilities
 print(info.instruction)
 point = True
 while point:
+    print("\nYou are in main menu!")
     get_request = input('What do you want? - ')
+
     if get_request == '/register':
         while True:
             reg_login = input('Write your login - ')
+            if reg_login == '/return':
+                break
             reg_password = input('Write your password - ')
+            if reg_password == '/return':
+                break
             reg_password_1 = input('Write your password again - ')
+            if reg_password_1 == '/return':
+                break
+
             if not utilities.chek_login(reg_login):
                 if reg_password == reg_password_1:
                     utilities.user_registration(reg_login, reg_password)
@@ -24,7 +33,12 @@ while point:
     elif get_request == "/login":
         while True:
             log_login = input("Write login - ")
+            if log_login == '/return':
+                break
             log_password = input("Write password - ")
+            if log_password == '/return':
+                break
+
             if utilities.check_log_pass(log_login, log_password):
                 print("You are successfully logged in!")
                 point = False
@@ -35,8 +49,11 @@ while point:
     elif get_request == '/help':
         print(info.instruction)
 
+    elif get_request == '/return':
+        print('You are in main menu, you cannot return back!')
+
     elif get_request == '/end':
-        print("GoodBye")
+        print("GoodBye!")
         break
 
     else:
